e2e4ecd2abe46cf070117e34b13fc13df113d804
Metroid-X <cjbells3002@gmail.com>
2024-12-13 02:12:50 -0500

    commit new ui changes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,32 @@
-const gameElem = document.querySelector('body');
+const gameElem = document.querySelector('#game-box');
+const bodyElem = document.querySelector('body');
 
+const buttonElems = {
+    op1: document.querySelector('#op-1'),
+    op2: document.querySelector('#op-2'),
+    op3: document.querySelector('#op-3'),
+    op4: document.querySelector('#op-4'),
+    dLogBox: document.querySelector('#d-log-box'),
+}
+
+gameElem.addEventListener('click',(e)=>{
+
+    let evTarg = e.target;
+
+    if(evTarg.parentElement.parentElement.id=='text-boxes'){
+        switch(evTarg.parentElement.id){
+            case 'op-1':
+                console.log(1)
+                break;
+            case 'op-2':
+                console.log(2)
+                break;
+            case 'op-3':
+                console.log(3)
+                break;
+            case 'op-4':
+                console.log(4)
+                break;
+        }
+    }
+})
